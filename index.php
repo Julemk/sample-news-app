319905f24dd5b0d659d094a3058a0810e447efc7
Maciej Czerwiński <53826753+Julemk@users.noreply.github.com>
2021-06-04 18:18:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,24 +13,6 @@ $query_builder = TRUE;
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 
-
-if (count($_FILES) > 0) {
-    if (is_uploaded_file($_FILES['userImage']['tmp_name'])) {
-        require_once "db.php";
-        $imgData = addslashes(file_get_contents($_FILES['userImage']['tmp_name']));
-        $imageProperties = getimageSize($_FILES['userImage']['tmp_name']);
-        
-        $sql = "INSERT INTO output_images(imageType ,imageData)
-	VALUES('{$imageProperties['mime']}', '{$imgData}')";
-        $current_id = mysqli_query($conn, $sql) or die("<b>Error:</b> Problem on Image Insert<br/>" . mysqli_error($conn));
-        if (isset($current_id)) {
-            header("Location: listImages.php");
-        }
-    }
-}
-
-
-
 include_once("index.html");
 
 ?>
